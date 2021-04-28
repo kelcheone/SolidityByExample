8fce474fa3a100cef80a284067757a0e9ab6bf35
kelcheone <kelcheone@gmail.com>
2021-04-28 19:16:21 +0300

    IfElse.sol contracta and deployed

---
+++ contracts/IfElse.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+contract IfElse {
+    function foo(uint256 x) public pure returns (uint256) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
--- migrations/2_contracts_migrations.js
+++ migrations/2_contracts_migrations.js
@@ -1,8 +1,10 @@
 const EtherUnits = artifacts.require("EtherUnits");
 const Gas = artifacts.require("Gas");
+const IfElse = artifacts.require("IfElse");
 
 
 module.exports= function(deployer){
     deployer.deploy(EtherUnits);
     deployer.deploy(Gas);
+    deployer.deploy(IfElse);
 };
