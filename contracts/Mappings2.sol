c7e969ffa0af83a54296a283d1d8288406578dd5
kelcheone <kelcheone@gmail.com>
2021-05-02 16:33:08 +0300

    nested mappings and deployments

--- contracts/Mappings2.sol
+++ contracts/Mappings2.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.4;
 contract Map2 {
     mapping(uint256 => string) public names;
     mapping(uint256 => Book) public books;
+    mapping(address => mapping(uint256 => Book)) myBooks;
 
     struct Book {
         string title;
@@ -22,5 +23,13 @@ contract Map2 {
         string memory _author
     ) public {
         books[_id] = Book(_title, _author);
+    }
+
+    function addMyBook(
+        uint256 _id,
+        string memory _title,
+        string memory _author
+    ) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
     }
 }
