0235760efd1c8561a8e11b2ee9796194a3f36cda
kelcheone <kelcheone@gmail.com>
2021-05-03 12:26:58 +0300

    payable functions

--- contracts/Payable.sol
+++ contracts/Payable.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-contract Name {
-    // payablr address can receive Ether.
+contract Payable {
+    // payable address can receive Ether.
 
     address payable public owner;
 
